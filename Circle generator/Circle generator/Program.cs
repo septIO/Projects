2e493c4df117a6f8a7adc119e512bf95d6a514ee
septIO <in@sept.io>
2016-01-25 13:20:07 +0100

    First Commit

---
+++ Circle generator/Circle generator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_generator
+{
+    using static Console;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                SetCursorPosition(0, 0);
+                SetWindowSize(50, 50);
+                int x, y, radius, circ, area;
+
+                Write("Enter radius: ");
+                radius = int.Parse(ReadLine());
+
+                x = radius;
+                y = radius;
+
+                ForegroundColor = ConsoleColor.White;
+                for (int i = 0; i <= 360; i++)
+                {
+                    double h, k;
+
+                    h = Math.Ceiling(x + radius * Math.Cos(i));
+                    k = Math.Ceiling(y + radius * Math.Sin(i));
+                    SetCursorPosition((int)h, (int)k);
+
+                    Write("■");
+                }
+
+                ReadKey();
+                Clear();
+            }
+        }
+    }
+}
---
+++ snake/snake/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    using static Console;
+    class Program
+    {
+        static char snake = '■';
+        static char food = '#';
+
+        static ConsoleColor foodColor = ConsoleColor.Yellow;
+        static int gridSize = 20;
+        static ConsoleKey direction;
+
+        static Random randomGenerator = new Random();
+
+        static void Main(string[] args)
+        {
+            int curX, curY;
+            //CursorVisible = false;
+            curX = 10; //Starting positions
+            curY = 10; //Starting positions
+
+            spawnFood(curX, curY);
+            SetCursorPosition(0, 0);
+            Write(new string(snake, 4));
+
+            ConsoleKey up = ConsoleKey.UpArrow;
+            ConsoleKey down = ConsoleKey.DownArrow;
+            ConsoleKey left = ConsoleKey.LeftArrow;
+            ConsoleKey right = ConsoleKey.RightArrow;
+
+            ConsoleKeyInfo cki;
+            while (true)
+            {
+                cki = Console.ReadKey();
+                if (cki.Key == up ||
+                    cki.Key == right ||
+                    cki.Key == down ||
+                    cki.Key == left) direction = cki.Key;
+                    
+            }
+
+            ReadKey();
+
+        }
+
+        static void spawnFood(int x, int y)
+        {
+            int fX = randomGenerator.Next(gridSize + 1);
+            int fY = randomGenerator.Next(gridSize + 1);
+            SetCursorPosition(fX, fY);
+
+            ForegroundColor = foodColor;
+            Write(food);
+            
+            ForegroundColor = ConsoleColor.White;
+        }
+
+        static void move()
+        {
+
+        }
+    }
+}
